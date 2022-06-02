51e72e5a3b52bd3910637d1e02090d4e8ff79da6
Leonardo Conti <eng.leonardoconti@gmail.com>
2022-06-02 16:16:40 -0300

    search-bar-&-fixed-seed-file

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,4 +11,12 @@ class Game < ApplicationRecord
   # added for ManiGames Geocoding
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  # added for search
+  include PgSearch::Model
+  pg_search_scope :search_by_all,
+                  against: %i[title console genre brand address],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
