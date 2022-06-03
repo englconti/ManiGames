bd93386c9c12d883cbdf997d4c326cc6697e4b8e
Leonardo Conti <eng.leonardoconti@gmail.com>
2022-06-03 12:55:41 -0300

    games-index-fixed-and-default-available-to-true-for-new-games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_010631) do
+ActiveRecord::Schema.define(version: 2022_06_03_154446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2022_06_03_010631) do
   create_table "games", force: :cascade do |t|
     t.integer "daily_rent"
     t.integer "min_rent_period"
-    t.boolean "available", default: false
+    t.boolean "available", default: true
     t.string "title"
     t.integer "released_year"
     t.string "brand"
